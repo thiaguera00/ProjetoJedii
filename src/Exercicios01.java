284709d132a5a6fb23c6cf8eac3987ad2668290d
thiaguera00 <thiagocaetano155@gmail.com>
2022-02-10 12:02:17 -0300

    Nivel Padawan concluido

---
+++ src/Exercicios01.java
@@ -0,0 +1,13 @@
+ //Declarar uma variável int numero dentro do escopo do método main e
+//na linha seguinte atribuir um valor para ela. Após isso,
+//usar o método System.out.println() para exibir o valor da variável no console.
+//OBS*: Se atentar ao parâmetro do método de exibição
+
+
+public class Exercicios01 {
+    public static void main(String[] args){
+        int number;
+        number = 10;
+        System.out.println(number);
+    }
+}
